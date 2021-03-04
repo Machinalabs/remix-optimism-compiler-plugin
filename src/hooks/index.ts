7e721bf5daa2cbd15375d0a88c9183665e1bac9f
Edson Alcala <edson_alcala@msn.com>
2021-03-04 14:28:51 +0000

    added web worker support

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from "./useRemix"
+export * from './useCompilerLoader'
---
+++ src/workers/compiler.worker.ts
@@ -0,0 +1,36 @@
+var wrapper = require('@eth-optimism/solc/wrapper');
+
+let compileJSON: ((input: any) => string) | null = (input) => { return "" }
+
+export const loadCompiler = (url: string) => {
+    try {
+        /* eslint-disable-next-line no-restricted-globals */
+        delete (self as any).Module;
+        /* eslint-disable-next-line no-restricted-globals */
+        (self as any).Module = undefined
+
+        compileJSON = null;
+        importScripts(url)
+        /* eslint-disable-next-line no-restricted-globals */
+        const compiler = wrapper((self as any).Module)
+
+        compileJSON = (input) => {
+            try {
+                const result = compiler.compile(input)
+                return result
+            } catch (exception) {
+                return JSON.stringify(exception)
+            }
+        }
+    } catch (error) {
+        console.log("Error in worker", error)
+    }
+}
+
+export const compile = async (input: any) => {
+    if (compileJSON) {
+        const result = compileJSON(input)
+        return result
+    }
+}
+
