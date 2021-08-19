9c98cdc774bda12b771f634af2de51df4258c82a
bunsenstraat <filip.mertens@ethereum.org>
2021-08-19 12:57:13 +0200

    add custom action

--- src/hooks/RemixClient.ts
+++ src/hooks/RemixClient.ts
@@ -1,4 +1,5 @@
 import { PluginClient } from "@remixproject/plugin"
+import { customAction } from "@remixproject/plugin-api"
 import { createClient } from "@remixproject/plugin-webview"
 
 export class RemixClient extends PluginClient {
@@ -6,12 +7,16 @@ export class RemixClient extends PluginClient {
 
   constructor() {
     super()
-
+    this.methods =  ["compileCustomAction"]
     createClient(this)
   }
 
   public setCompilerCallback(compileFunc: any) {
     this.compilerCallback = compileFunc
+  }
+
+  public async compileCustomAction(action: customAction) {
+    if (action.path[0]) await this.compile(action.path[0])
   }
 
   public async compile(fileName: string) {
